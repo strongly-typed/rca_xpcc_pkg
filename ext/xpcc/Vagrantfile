b3e0f532ab902778f8b5cb01a34de76e22889c35
strongly-typed <stronglytyp3d@gmail.com>
2015-02-19 12:00:00 +0100

    Remove git subtree of xpcc as subtree is not available in stock Ubuntu 12.04.1

--- ext/xpcc/Vagrantfile
+++
@@ -1,7 +0,0 @@
-# -*- mode: ruby -*-
-# vi: set ft=ruby :
-
-Vagrant.configure("2") do |config|
-  config.vm.box = "xpcc"
-  config.vm.box_url = "http://box.xpcc.io/xpcc-vm.box"
-end
--- ext/xpcc/src/xpcc/ui/menu/choice_menu_entry.cpp
+++
@@ -1,8 +0,0 @@
-#include "choice_menu_entry.hpp"
-
-
-xpcc::ChoiceMenuEntry::ChoiceMenuEntry(const char* text, uint16_t space, bool *valuePtr, bool defaultValue):
-	text(text, space), valuePtr(valuePtr), defaultValue(defaultValue)
-{
-
-}
